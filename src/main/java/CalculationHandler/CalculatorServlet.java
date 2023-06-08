4e127cc6e08cbbf12686b9c79a8754fd8137630b
Tarun8357 <tarun.dhakad1205@gmail.com>
2023-06-08 18:07:02 +0530

    Project Committed

---
+++ src/main/java/CalculationHandler/CalculatorServlet.java
@@ -0,0 +1,42 @@
+package CalculationHandler;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+@WebServlet("/calc")
+public class CalculatorServlet extends HttpServlet {
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.setHeader("Access-Control-Allow-Origin",  "*");
+    resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+    PrintWriter out = resp.getWriter();
+    resp.setContentType("application/json");
+    Gson gson = new Gson();
+    StringBuilder stringBuilder = new StringBuilder();
+    BufferedReader bufferedReader = req.getReader();
+    String line;
+    while((line= bufferedReader.readLine())!=null){
+      stringBuilder.append(line);
+    }
+    String payload = stringBuilder.toString();
+    ArrayList<CalculatorDTO> paramList = gson.fromJson(payload, new TypeToken<ArrayList<CalculatorDTO>>(){}.getType());
+    out.print(CalculatorModel.calculation(paramList));
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      doPost(req, resp);
+      resp.sendRedirect("");
+    super.doGet(req, resp);
+  }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,29 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags"%>
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:th="http://www.thymeleaf.org">
+
+<head>
+    <title>Hello Security</title>
+</head>
+<body>
+<div>
+    <h1>Hello Security</h1>
+
+  <security:authorize access="hasRole('ADMIN')">
+  <p>Logged in as admin</p>
+  </security:authorize>
+
+  <security:authorize access="hasRole('USER')">
+    <p>Logged in as user</p>
+    </security:authorize>
+
+    <form th:action="@{/logout}" method="post">
+
+    <security:csrfInput />
+        <input type="submit" value="Logout"/>
+        <a href="http://localhost:8080/calculator/">click</a>
+    </form>
+</div>
+</body>
+</html>
