fdbdd73f56eb80323b03070156c0af8ec0f26ca9
Tarun8357 <tarun.dhakad1205@gmail.com>
2023-06-08 18:06:21 +0530

    Project Committed

--- src/main/java/SecurityConfigurations/SecurityConfigs.java
+++ src/main/java/SecurityConfigurations/SecurityConfigs.java
@@ -1,17 +1,25 @@
 package SecurityConfigurations;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -32,13 +40,26 @@ public class SecurityConfigs {
     }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http ) throws Exception {
-        http.csrf().and().cors().disable();
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and().cors().disable();
+
 
         http.authorizeHttpRequests()
-                .requestMatchers("/login").hasRole("admin")
-                .requestMatchers("/**").authenticated()
+//                .requestMatchers("/login").hasRole("ADMIN")
+                .requestMatchers("/calc").hasRole("ADMIN")
+                .requestMatchers("/**")
+                .authenticated()
                 .and()
-                .formLogin().permitAll();
+                .formLogin().permitAll().successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+                        String username = userDetails.getUsername();
+
+                        System.out.println("The user " + username + " has logged in.");
+
+                        response.sendRedirect(request.getContextPath());
+                    }
+                });
 
         return http.build();
     }
